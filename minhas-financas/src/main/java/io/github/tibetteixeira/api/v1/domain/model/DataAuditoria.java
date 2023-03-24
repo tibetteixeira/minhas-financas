bd854924cb3dea7587ff1e8e2e6526549834228b
Tibet Teixeira <tibetbrasil@gmail.com>
2023-03-24 13:08:05 -0300

    Ajuste campo timestamp

--- minhas-financas/src/main/java/io/github/tibetteixeira/api/v1/domain/model/DataAuditoria.java
+++ minhas-financas/src/main/java/io/github/tibetteixeira/api/v1/domain/model/DataAuditoria.java
@@ -5,6 +5,8 @@ import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import java.util.Date;
 
 @Getter
@@ -13,11 +15,14 @@ import java.util.Date;
 public class DataAuditoria {
 
     @Column(name = "data_criacao")
+    @Temporal(TemporalType.TIMESTAMP)
     private Date dataCriacao;
 
     @Column(name = "data_atualizacao")
+    @Temporal(TemporalType.TIMESTAMP)
     private Date dataAtualizacao;
 
     @Column(name = "data_exclusao")
+    @Temporal(TemporalType.TIMESTAMP)
     private Date dataExclusao;
 }
