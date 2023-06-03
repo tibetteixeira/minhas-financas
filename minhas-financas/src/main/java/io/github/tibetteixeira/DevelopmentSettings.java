f2bea79df4beec9719aa1933b0bd1f2df4d4601b
Tibet Teixeira <tibetbrasil@gmail.com>
2023-06-03 20:25:51 -0300

    Ajuste configuração

--- minhas-financas/src/main/java/io/github/tibetteixeira/DevelopmentSettings.java
+++ minhas-financas/src/main/java/io/github/tibetteixeira/DevelopmentSettings.java
@@ -4,11 +4,15 @@ import io.github.tibetteixeira.annotations.Development;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 
+import java.util.logging.Logger;
+
 @Development
-public class DevelopmentConfiguration {
+public class DevelopmentSettings {
+
+    private static final Logger LOGGER = Logger.getLogger(DevelopmentSettings.class.getName());
 
     @Bean
     public CommandLineRunner initialize() {
-        return args -> System.out.println("Aplicação em ambiente de desenvolvimento disponível.");
+        return args -> LOGGER.info("Aplicação em ambiente de desenvolvimento disponível.");
     }
 }
