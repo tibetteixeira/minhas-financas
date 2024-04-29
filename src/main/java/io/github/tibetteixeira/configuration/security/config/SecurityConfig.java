eb1b2b309b7907dfbcbc54ab1fcbd4080bf68426
Tibet Teixeira <tibetbrasil@gmail.com>
2024-04-28 22:59:50 -0300

    Atualização do Swagger

--- src/main/java/io/github/tibetteixeira/configuration/security/config/SecurityConfig.java
+++ src/main/java/io/github/tibetteixeira/configuration/security/config/SecurityConfig.java
@@ -26,8 +26,8 @@ public class SecurityConfig {
     private final LogoutHandler logoutHandler;
 
     private static final String[] AUTH_WHITELIST = {
-            "/v2/api-docs",
-            "/v3/api-docs",
+            "/v2/api-docs/**",
+            "/v3/api-docs/**",
             "/configuration/ui",
             "/swagger-resources/**",
             "/configuration/security",
